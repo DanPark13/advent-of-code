c224139a0bd83f98c9fbf68a59b79b80418227e6
DanPark13 <dpark0703@gmail.com>
2022-12-06 12:04:14 -0500

    Day Six Complete

--- day-6/day-6-solution.py
+++ day-6/day-6-solution.py
@@ -3,19 +3,24 @@
 with open("input.txt","r",encoding="utf-8") as infile:
     data = infile.read()
 
-current_index = 0
-len_of_stack = 0
+def read_chars(digit):
+    current_index = 0
+    len_of_stack = 0
+    checked = []
 
-checked = []
+    for char in data:
+        if char in checked:
+            checked = checked[checked.index(char)+1:]
 
-for char in data:
-    if char in checked:
-        checked = checked[checked.index(char)+1:]
+        checked.append(char)
+        current_index += 1
 
-    checked.append(char)
-    current_index += 1
+        if len(checked) == digit:
+            break
+    return current_index
 
-    if len(checked) == 4: # Replace 4 with 14 for Part II
-        break
+# Part I
+print(read_chars(4))
 
-print(current_index)
+# Part II
+print(read_chars(14))
